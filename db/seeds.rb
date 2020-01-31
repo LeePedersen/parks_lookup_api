1feb6325d1aed60fd83b4bceac18046ee9891f8d
Lee Pedersen <leepedersenr@gmail.com>
2020-01-31 09:29:52 -0800

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+  Park.destroy_all
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_parks
+  end
+
+  def generate_parks
+    types = ["National Park", "National Monument"]
+    10.times do |i|
+      type = "National Trail"
+      if i % 2 == 0
+        type = types[0]
+      else
+        type = types[1]
+      end
+      park = Park.create!(name: Faker::Nation.capital_city, location: Faker::Address.state, type: type, description: Faker::Lorem.paragraph(sentence_count: 4))
+      puts "Park #{i}: name is #{park.name}."
+    end
+  end
+
+end
+
+Seed.begin
